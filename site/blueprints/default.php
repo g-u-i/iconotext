4a690c562e3bc0452e3901ca9f756c19db0e8feb
TanguyW <tanguy.wermelinger@gmail.com>
2016-03-16 17:43:50 +0100

    Blueprint à jour

--- site/blueprints/default.php
+++ site/blueprints/default.php
@@ -3,10 +3,18 @@
 title: Page
 pages: true
 files: true
+  fields:
+    caption:
+      label: Légendes des images dans le diaporama
+      type: textarea
 fields:
   title:
-    label: Title
+    label: Titre de la section
+    type:  text
+  subtitle:
+    label: Sous titre de la section
     type:  text
   text:
-    label: Text
+    label: On verra à quoi ça sert
     type:  textarea
+    size:  large
--- site/templates/default.php
+++ site/templates/default.php
@@ -51,7 +51,7 @@
 				</div>
 		        <?php foreach($page->images()->sortBy('sort', 'asc') as $image): ?>
 		            <div class="slide">
-		            	<h1 class="sub"><?php echo $image->text(); ?></h1>
+		            	<h1 class="sub"><?php echo $image->caption(); ?></h1>
 		              	<img src="<?php echo $image->url() ?>" alt="<?php echo $page->title()->html() ?>">
 		            </div>
 		        <?php endforeach ?>
