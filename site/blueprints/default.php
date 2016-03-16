ed6ed6ee5b5419990e70eb49f9ac41e2462da164
TanguyW <tanguy.wermelinger@gmail.com>
2016-03-16 18:53:34 +0100

    adding sortable files + media

--- site/blueprints/default.php
+++ site/blueprints/default.php
@@ -3,18 +3,19 @@
 title: Page
 pages: true
 files: true
+  sortable: true
   fields:
-    caption:
-      label: Légendes des images dans le diaporama
-      type: textarea
+  caption:
+    label: Légendes des images dans le diaporama
+    type: textarea
 fields:
   title:
-    label: Titre de la section
-    type:  text
+   label: Titre de la section
+   type:  text
   subtitle:
-    label: Sous titre de la section
-    type:  text
+   label: Sous titre de la section
+   type:  text
   text:
-    label: On verra à quoi ça sert
-    type:  textarea
-    size:  large
+   label: On verra à quoi ça sert
+   type:  textarea
+   size:  large
