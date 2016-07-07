81fa24afba47cd79f0378d3b9baa42c9d70ab32e
dcfvg <qwsx@dcfvg.com>
2016-07-07 18:18:51 +0200

    improving documentation website

--- site/templates/home.php
+++ site/templates/home.php
@@ -2,7 +2,9 @@
 
 
   <div class="container-fluid ">
-    <h1 class="navbar-fixed-top logo"><?php echo  $page->title()->html()?></h1>
+    <h1 class="navbar-fixed-top logo">
+    <img src="/assets/images/iconologo.svg">
+    <?php // echo  $page->title()->html() ?></h1>
   </div>
 
 
@@ -11,7 +13,7 @@
     <div class="lead col-sm-8 col-sm-offset-2"><?php echo  $page->subtitle()->kirbytext()?></div>
 
   </div>
-  <div class="container">
+  <div class="container maincontent">
     <?php echo $page->text()->kirbytext() ?>
   </div>
 
